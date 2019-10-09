da9f4d7d85f42083e496b001af3bb15e6a4f9271
rogeto20 <rogeto20@jcu.edu>
2019-10-09 14:09:35 -0400

    Added a Quotes class

---
+++ Quotable/quotable.core/Quotes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quotable.core
+{
+    class Quotes
+    {
+        public int ID { get; set; }
+        public string Quote { get; set; }
+        public string Author { get; set; }
+    }
+}
