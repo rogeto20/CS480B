4733d32ebfccedc92c7c693b2bd9dacd799f4299
rogeto20 <rogeto20@jcu.edu>
2019-10-10 17:13:01 -0400

    Added random quote functionality

---
+++ Quotable/quotable.api/Controllers/RandomController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using quotable.core;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace quotable.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RandomController : Controller
+    {
+        SimpleRandomQuoteProvider randomQuoteProvider = new SimpleRandomQuoteProvider();
+        // GET: api/<controller>
+        [HttpGet]
+        public Quotes Get()
+        {
+            int numberOfQuotes = randomQuoteProvider.getQuotes().Count();
+            Random random = new Random();
+            int rand = random.Next(0, numberOfQuotes);
+
+            return randomQuoteProvider.getQuotesbyID(rand);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
